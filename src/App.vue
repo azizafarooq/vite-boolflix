16803f80af11039762c7a41ab226f74940a4efe1
azizafarooq <azizafarooq2000@gmail.com>
2024-02-17 16:21:49 +0100

    feat: AppMain

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import axios from 'axios';
 import { store } from './store';
 import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 export default {
   data() {
     return {
@@ -53,48 +54,16 @@ export default {
     performSearch(searchedTerm) {
       this.fetchMovies(searchedTerm);
       this.fetchTvSeries(searchedTerm);
-    },
-
-    getFlag(langCode) {
-      if (langCode == 'it') {
-        return '/images/square.png'
-      }
-
-      if (langCode == 'en') {
-        return '/images/united-kingdom.png'
-      }
     }
   },
 
-  components: { AppHeader }
+  components: { AppHeader, AppMain }
 }
 </script>
 
 <template>
   <AppHeader @search="performSearch"></AppHeader>
-
-  <div class="mt-5 container">
-    <h2>Film</h2>
-    <ul v-for="movie in store.movies">
-      <li>Titolo: {{ movie.title }}</li>
-      <li>Titolo Originale: {{ movie.original_title }}</li>
-      <li>Lingua:
-        <img :src="getFlag(movie.language)" alt="">
-      </li>
-      <li>Voto: {{ movie.vote }}</li>
-    </ul>
-    <hr>
-    <h2>Serie Tv</h2>
-    <!--<ul v-for="series in store.tvSeries">
-      <li>Titolo: {{ series.name }}</li>
-      <li>Titolo Originale: {{ series.original_title }}</li>
-      <li>Lingua:
-        <img :src="getFlag(series.language)" alt="">
-      </li>
-      <li>Voto: {{ series.vote }}</li>
-    </ul>-->
-
-  </div>
+  <AppMain></AppMain>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppMain.vue
@@ -0,0 +1,48 @@
+<script>
+import { store } from '../store';
+export default {
+    data() {
+        return {
+            store,
+        }
+    },
+
+    methods: {
+        getFlag(langCode) {
+            if (langCode == 'it') {
+                return '/images/square.png'
+            }
+
+            if (langCode == 'en') {
+                return '/images/united-kingdom.png'
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="mt-5 container">
+        <h2>Film</h2>
+        <ul v-for="movie in store.movies">
+            <li>Titolo: {{ movie.title }}</li>
+            <li>Titolo Originale: {{ movie.original_title }}</li>
+            <li>Lingua:
+                <img :src="getFlag(movie.language)" alt="">
+            </li>
+            <li>Voto: {{ movie.vote }}</li>
+        </ul>
+        <hr>
+        <h2>Serie Tv</h2>
+        <!--<ul v-for="series in store.tvSeries">
+      <li>Titolo: {{ series.name }}</li>
+      <li>Titolo Originale: {{ series.original_title }}</li>
+      <li>Lingua:
+        <img :src="getFlag(series.language)" alt="">
+      </li>
+      <li>Voto: {{ series.vote }}</li>
+    </ul>-->
+
+    </div>
+</template>
+<style lang="scss" scoped></style>
