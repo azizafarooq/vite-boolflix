543f6e4e0ec321515125e09d3554dadb94a546b3
azizafarooq <azizafarooq2000@gmail.com>
2024-02-17 15:05:14 +0100

    feat: movies api and flag

--- src/App.vue
+++ src/App.vue
@@ -10,19 +10,91 @@ export default {
   },
 
   methods: {
+    fetchMovies() {
+      axios
+        .get(this.store.api.uri + 'search/movie', {
+          params: {
+            api_key: store.api.key,
+            query: this.searchedTerm,
+          }
+        })
+        .then((response) => {
+          store.movies = response.data.results.map((movie) => {
+            return {
+              title: movie.title,
+              original_title: movie.original_title,
+              language: movie.original_language,
+              vote: movie.vote_average
+            }
+          });
+        })
+    },
+
+    fetchTvSeries() {
+      axios
+        .get(this.store.api.uri + 'search/tv', {
+          params: {
+            api_key: store.api.key,
+            query: this.searchedTerm,
+          }
+        })
+        .then((response) => {
+          store.tvSeries = response.data.results.map((series) => {
+            return {
+              title: series.title,
+              original_title: series.original_title,
+              language: series.original_language,
+              vote: series.vote_average
+            }
+          });
+        })
+    },
+
     performSearch() {
-      axios.get().then((res) => {
-        console.log(res.data);
-      })
+      this.fetchMovies();
+      this.fetchTVSeries();
+    },
+
+    getFlag(langCode) {
+      if (langCode == 'it') {
+        return '/images/square.png'
+      }
+
+      if (langCode == 'en') {
+        return '/images/united-kingdom.png'
+      }
     }
   }
 }
 </script>
 
 <template>
-  <div class="container mt-5">
+  <div class="container mt-5 d-flex">
     <input type="text" class="form-control" v-model="searchedTerm" @keyup.enter="performSearch()">
-    <button class="btn btn-primary" @click="performSearch()">Ricerca</button>
+    <button class="btn btn-primary ms-3" @click="performSearch()">Ricerca</button>
+  </div>
+
+  <hr>
+
+  <div class="mt-5">
+    <ul v-for="movie in store.movies">
+      <li>Titolo: {{ movie.title }}</li>
+      <li>Titolo Originale: {{ movie.original_title }}</li>
+      <li>Lingua:
+        <img :src="getFlag(movie.language)" alt="">
+      </li>
+      <li>Voto: {{ movie.vote }}</li>
+    </ul>
+
+    <!-- <ul v-for="series in tvSeries.movies">
+      <li>Titolo: {{ series.title }}</li>
+      <li>Titolo Originale: {{ series.original_title }}</li>
+      <li>Lingua:
+        <img :src="getFlag(series.language)" alt="">
+      </li>
+      <li>Voto: {{ series.vote }}</li>
+    </ul> -->
+
   </div>
 </template>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,11 @@
 import { reactive } from 'vue';
 export const store = reactive({
-    apiUri,
+    api: {
+        key: '08d0c644d8c6f8b417ef65919608b54c',
+        uri: 'https://api.themoviedb.org/3/',
+    },
+
+    movies: [],
+    tvSeries: [],
+
 });
